5cd1fdc41167ac9f5f002f3e07f41df6b43b112b
Veg_chic <h_foryou@163.com>
2018-06-19 15:46:33 +0800

    添加logger, eslint, nodemon

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const path = require('path');
-const koa = require('koa');
+const Koa = require('koa');
 const serve = require('koa-static');
-const view = require('koa-views');
+const views = require('koa-views');
 const bodyParser = require('koa-bodyparser');
+const logger = require('koa-logger');
 
-const app = new koa();
+const app = new Koa();
+
+app.use(logger());
 
 app.use(bodyParser());
 
-app.use(serve(path.join(__dirname + './public')));
+app.use(serve(path.join(__dirname, './public')));
 
-app.use(view(path.join(__dirname, './views')));
+// app.use(views(path.join(__dirname, './views'), {
+//   extension: 'html',
+// }));
 
-app.listen(8800);
+app.listen(8080);
